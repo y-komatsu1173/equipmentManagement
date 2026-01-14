a3219c1070d890b25a36658c1cdba44a5dd6a405
komatsu1173 <yu85wave@gmail.com>
2026-01-14 11:46:54 +0900

    fix:IndexController.java データが取得できているかの確認処理を追加

--- equipment/src/main/java/jp/co/sss/equipment/controller/IndexController.java
+++ equipment/src/main/java/jp/co/sss/equipment/controller/IndexController.java
@@ -46,6 +46,23 @@ public class IndexController {
 		List<DetailListViewDto> detailList = indexService.detailFind(name);//サービス層のdetailFindメソッドを呼び出し値をリストに返す
 		model.addAttribute("detailName", detailList.get(0)); //備品名が複数取得されるため１つ目だけ採取しHTMLに反映（１つだけのため）
 		model.addAttribute("itemDetail", detailList); 
+		//デバッグ
+		int num = 0;
+		for (DetailListViewDto i : detailList) {
+			num++;
+			System.out.println("==========="+num+"===========");
+			System.out.println("===========" + "表示" + "===========");
+			System.out.println("備品名:"+i.getEquipmentName());
+			System.out.println("シリアル:"+i.getParentStockCode());
+			System.out.println("使用者:"+i.getStaffName());
+			System.out.println("貸出可否:"+i.getRentFlg());
+			System.out.println("貸出開始日:"+i.getStartDate());
+			System.out.println("返却予定日:"+i.getLimitDate());
+			System.out.println("最終所在確認"+i.getConfirmedDate());
+			System.out.println("備考:"+i.getRemarks());
+		    System.out.println("シーケンスID: " + i.getEquipmentId());
+		    System.out.println("スタッフID:"+i.getStaffNo());
+		}
 		return "index/detail";
 	}
 	
