0baa3f678c70ebbbf344474d1529207a23d0b0f1
komatsu1173 <yu85wave@gmail.com>
2026-01-14 11:52:48 +0900

    fix:BorrowingService.java 貸出処理を追加

--- equipment/src/main/java/jp/co/sss/equipment/service/BorrowingService.java
+++ equipment/src/main/java/jp/co/sss/equipment/service/BorrowingService.java
@@ -1,9 +1,12 @@
 package jp.co.sss.equipment.service;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import jp.co.sss.equipment.dto.DetailListViewDto;
 import jp.co.sss.equipment.entity.StaffData;
@@ -26,12 +29,32 @@ public class BorrowingService {
 	public List<DetailListViewDto> borrowingFindView(String name) {
 		return borrowingMapper.borrowingFind(name); //マッパーの処理を返す
 	}
-	
+
 	/**
 	 * スタッフデータを取得
 	 * @return
 	 */
 	public List<StaffData> staffDataFind() {
 		return borrowingMapper.staffFind();//マッパーの処理を返す
+	}
+
+	/**
+	 * 貸出更新
+	 */
+	@Transactional
+	public void borrowingEquipment(
+	        List<Integer> equipmentIdList,
+	        Map<String, String> staffNoMap,
+	        Map<String, String> startDateMap,
+	        Map<String, String> limitDateMap) {
+
+	    for (Integer id : equipmentIdList) {
+	        // HTML で生成される name に合わせて Map から値を取得
+	        Integer staffNo = Integer.valueOf(staffNoMap.get("staffNoMap[" + id + "]"));
+	        LocalDate startDate = LocalDate.parse(startDateMap.get("startDateMap[" + id + "]"));
+	        LocalDate limitDate = LocalDate.parse(limitDateMap.get("limitDateMap[" + id + "]"));
+
+	        borrowingMapper.borrowingUpdate(id, staffNo, startDate, limitDate);
+	    }
 	}
 }
