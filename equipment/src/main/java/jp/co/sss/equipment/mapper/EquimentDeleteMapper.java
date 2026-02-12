524bb46b299a597b49db48800f6ea472f9ec6757
komatsu1173 <yu85wave@gmail.com>
2026-02-12 11:13:03 +0900

    add:EquimentDeleteMapper 論理削除を行うMapperを追加

---
+++ equipment/src/main/java/jp/co/sss/equipment/mapper/EquimentDeleteMapper.java
@@ -0,0 +1,17 @@
+package jp.co.sss.equipment.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.data.repository.query.Param;
+
+/**
+ * 備品削除マッパー
+ * @author 小松原
+ *
+ */
+@Mapper
+public interface EquimentDeleteMapper {
+	/**
+	 * 削除処理(論理削除)
+	 */
+	void deleteBySerialNo(@Param("serialNo") String serialNo);
+}
