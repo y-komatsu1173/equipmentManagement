74647bbca2c449d975f70e6dd7e4658d56ebffb7
komatsu1173 <yu85wave@gmail.com>
2026-02-17 09:44:15 +0900

    fix:IndexController.java 個別詳細用のコンソール表示を削除

--- equipment/src/main/java/jp/co/sss/equipment/controller/IndexController.java
+++ equipment/src/main/java/jp/co/sss/equipment/controller/IndexController.java
@@ -67,25 +67,6 @@ public class IndexController {
 	public String individualDetail(Model model, String serialNo) {
 		DetailListViewDto detailList = indexService.serialNoFind(serialNo);
 		DetailListViewDto detail = detailList;
-		//値チェック
-		System.out.println("=========個別詳細画面=========");
-		System.out.println("備品名:"+detail.getEquipmentName());
-		System.out.println("カテゴリ番号:" + detail.getStockType());
-		System.out.println("シリアルNo:"+detail.getParentStockCode());
-		System.out.println("使用者："+detail.getStaffName());
-		System.out.println("貸出可否："+detail.getRentFlg());
-		System.out.println("貸出開始日"+detail.getStartDate());
-		System.out.println("返却予定日:"+detail.getLimitDate());
-		System.out.println("最終所在確認日："+detail.getConfirmedDate());
-		System.out.println("親シリアル："+detail.getConfirmedDate());
-		System.out.println("製品名："+detail.getProductName());
-		System.out.println("メーカ名："+detail.getMaker());
-		System.out.println("分類："+detail.getOwnershipType());
-		System.out.println("リース返却日："+detail.getLeaseReturnDate());
-		System.out.println("備考："+detail.getRemarks());
-		System.out.println("シーケンスID"+detail.getEquipmentId());
-		System.out.println("スタッフID"+detail.getStaffNo());
-		System.out.println("貸出可否:" +detail.getRentFlag());
 		model.addAttribute("detailName", detailList); //備品名が複数取得されるため１つ目だけ採取しHTMLに反映（１つだけのため）
 		model.addAttribute("itemDetail", detailList); 
 		return "index/individualDetail";
