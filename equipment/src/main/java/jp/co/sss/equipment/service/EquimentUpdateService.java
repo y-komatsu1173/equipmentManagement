89307cbee3e42d4be55497f56a9061afee61977e
komatsu1173 <yu85wave@gmail.com>
2026-02-18 09:18:55 +0900

    fix:EquimentUpdateService.java　編集の更新処理を行うメソッドを追加

--- equipment/src/main/java/jp/co/sss/equipment/service/EquimentUpdateService.java
+++ equipment/src/main/java/jp/co/sss/equipment/service/EquimentUpdateService.java
@@ -7,6 +7,7 @@ import org.springframework.validation.BindingResult;
 import jp.co.sss.equipment.entity.StockMaster;
 import jp.co.sss.equipment.form.EquipmentForm;
 import jp.co.sss.equipment.mapper.EquimentUpdateMapper;
+import jp.co.sss.equipment.util.BeanCopy;
 import jp.co.sss.equipment.util.EquipmentInputCheck;
 
 /**
@@ -17,33 +18,45 @@ import jp.co.sss.equipment.util.EquipmentInputCheck;
 @Service
 public class EquimentUpdateService {
 
-    @Autowired
-    private EquimentUpdateMapper equimentUpdateMapper;
-    
-    @Autowired
+	@Autowired
+	private EquimentUpdateMapper equimentUpdateMapper;
+
+	@Autowired
 	EquipmentInputCheck equipmentInputCheck;
 
-    /*
-     * 備品編集のためのシリアルナンバーで検索
-     */
-    public StockMaster equimentFind(String serialNo) {
-        return equimentUpdateMapper.equimentFind(serialNo);
-    }
-    
-    /**
+	/*
+	 * 備品編集のためのシリアルナンバーで検索
+	 */
+	public StockMaster equimentFind(String serialNo) {
+		return equimentUpdateMapper.equimentFind(serialNo);
+	}
+
+	/**
 	 * 入力チェック
 	 * @param form
 	 * @param result
 	 */
 	public void registCheck(EquipmentForm form, BindingResult result) {
 
-        // 入力タイプ別チェック
+		// 入力タイプ別チェック
 		equipmentInputCheck.categoryCheck(form, result);
 
-        // ここでエラーあったら
-        if (result.hasErrors()) {
-            return;
-        }
-    }
-}
+		// ここでエラーあったら
+		if (result.hasErrors()) {
+			return;
+		}
+	}
 
+	/**
+	 * 備品更新
+	 * @param updateform
+	 * @param serialNo
+	 */
+	public void equipmentUpdate(EquipmentForm updateform, String serialNo) {
+		StockMaster stockMaster = BeanCopy.copyFormToStockMaster(updateform);
+		stockMaster.setStockCode(serialNo);
+		stockMaster.setDel("0");
+		equimentUpdateMapper.equipmentUpdate(stockMaster);
+
+	}
+}
