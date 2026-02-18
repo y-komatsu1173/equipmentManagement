1451d17e96dace8b25573582b5aa97b0bd612091
komatsu1173 <yu85wave@gmail.com>
2026-02-18 16:47:44 +0900

    fix:EquipmentUpdateController.java カテゴリチェックのif文を追加

--- equipment/src/main/java/jp/co/sss/equipment/controller/EquipmentUpdateController.java
+++ equipment/src/main/java/jp/co/sss/equipment/controller/EquipmentUpdateController.java
@@ -26,10 +26,10 @@ import jp.co.sss.equipment.util.EquipmentInputCheck;
 public class EquipmentUpdateController {
 	@Autowired
 	EquimentUpdateService equimentUpdateService;
-	
+
 	@Autowired
 	EquipmentRegistService equipmentRegistService;
-	
+
 	@Autowired
 	EquipmentInputCheck equipmentInputCheck;
 
@@ -46,21 +46,22 @@ public class EquipmentUpdateController {
 		EquipmentForm form = BeanCopy.copyDtoToUpdateForm(stockMaster);
 
 		model.addAttribute("categoryList", categoryList);
-	    model.addAttribute("equipmentForm", form);
-	    StockMaster updateform =equimentUpdateService.equimentFind(serialNo);
+		model.addAttribute("equipmentForm", form);
+		model.addAttribute("detailName", stockMaster);
+		StockMaster updateform = equimentUpdateService.equimentFind(serialNo);
 		//ログイン機能追加後は、セッションチェックを実装
 		return "equipmentUpdate/equipmentUpdateInput";
 	}
-	
+
 	/*
 	 * 備品編集確認画面
 	 */
 	@PostMapping("/equipmentUpdateCheck")
-	public String equipmentUpdateCheck(@ModelAttribute ("equipmentForm") EquipmentForm updateform,
+	public String equipmentUpdateCheck(@ModelAttribute("equipmentForm") EquipmentForm updateform,
 			BindingResult result,
 			RedirectAttributes redirectAttributes,
 			Model model) {
-		
+
 		StockTypeMaster stockTypeMaster = null;
 		//カテゴリIDからカテゴリ情報を取得
 		if (updateform.getCategoryId() != null) {
@@ -72,31 +73,34 @@ public class EquipmentUpdateController {
 
 		//入力チェック
 		equimentUpdateService.registCheck(updateform, result);
-		
+
+		if (updateform.getCategoryId() == null || stockTypeMaster == null) {
+			result.rejectValue("categoryId", "error.categoryId", "カテゴリが未選択です");
+		}
+
 		//エラーがある場合、入力画面へ戻る
-				if (result.hasErrors()) {
-				    List<StockTypeMaster> categoryList = equipmentRegistService.categoryFind();
-				    model.addAttribute("categoryList", categoryList);
-				    return "equipmentUpdate/equipmentUpdateInput"; 
-				}
-				
-				//カテゴリ名をモデルにセット
-				model.addAttribute("categoryName", stockTypeMaster.getName());
-				model.addAttribute("equipmentForm", updateform);
-		
+		if (result.hasErrors()) {
+			List<StockTypeMaster> categoryList = equipmentRegistService.categoryFind();
+			model.addAttribute("categoryList", categoryList);
+			return "equipmentUpdate/equipmentUpdateInput";
+		}
+
+		//カテゴリ名をモデルにセット
+		model.addAttribute("categoryName", stockTypeMaster.getName());
+		model.addAttribute("equipmentForm", updateform);
 		return "equipmentUpdate/equipmentUpdateCheck";
 	}
-	
+
 	/**
 	 * 備品登録入力画面へ戻る
 	 */
 	@PostMapping("/equipmentUpdateBack")
 	public String equipmentUpdateBack(@ModelAttribute EquipmentForm updateform, Model model) {
-	    //カテゴリの取得
-	    List<StockTypeMaster> categoryList = equipmentRegistService.categoryFind();
-	    model.addAttribute("categoryList", categoryList);
-	    model.addAttribute("equipmentForm", updateform);
-	    return "equipmentUpdate/equipmentUpdateInput";
+		//カテゴリの取得
+		List<StockTypeMaster> categoryList = equipmentRegistService.categoryFind();
+		model.addAttribute("categoryList", categoryList);
+		model.addAttribute("equipmentForm", updateform);
+		return "equipmentUpdate/equipmentUpdateInput";
 	}
 
 	/**
@@ -105,15 +109,15 @@ public class EquipmentUpdateController {
 	@PostMapping("/equipmentUpdateComplete")
 	public String equipmentUpdateComplete(EquipmentForm updateform, Model model) {
 		// リース返却日のNULLチェック
-	    equipmentInputCheck.leaseNullCheck(updateform);
+		equipmentInputCheck.leaseNullCheck(updateform);
 
-	    // stockCode 取得
-	    String serialNo = updateform.getStockCode();
+		// stockCode 取得
+		String serialNo = updateform.getStockCode();
 
-	    // 更新処理を Service に委譲
-	    equimentUpdateService.equipmentUpdate(updateform, serialNo);
-	    
-	    model.addAttribute("parentStockCode", updateform.getStockCode());
-	    return "equipmentUpdate/equipmentUpdateComplete";
+		// 更新処理を Service に委譲
+		equimentUpdateService.equipmentUpdate(updateform, serialNo);
+
+		model.addAttribute("parentStockCode", updateform.getStockCode());
+		return "equipmentUpdate/equipmentUpdateComplete";
 	}
 }
