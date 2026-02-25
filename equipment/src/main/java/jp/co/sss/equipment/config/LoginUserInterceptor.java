892e042a5b50861197760e551ce1ce6f5d98d1c6
komatsu1173 <yu85wave@gmail.com>
2026-02-25 14:21:25 +0900

    fix:LoginUserInterceptor.java loginCheckFilter()メソッドの追加

--- equipment/src/main/java/jp/co/sss/equipment/config/LoginUserInterceptor.java
+++ equipment/src/main/java/jp/co/sss/equipment/config/LoginUserInterceptor.java
@@ -4,8 +4,12 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
+
+import jp.co.sss.equipment.filter.LoginCheckFilter;
 
 @Component
 public class LoginUserInterceptor implements HandlerInterceptor {
@@ -18,5 +22,15 @@ public class LoginUserInterceptor implements HandlerInterceptor {
 	        request.setAttribute("loginUser", user);
 	    }
 	    return true;
+	}
+	
+	@Bean
+	public FilterRegistrationBean<LoginCheckFilter> loginCheckFilter() {
+	    FilterRegistrationBean<LoginCheckFilter> bean =
+	            new FilterRegistrationBean<>();
+
+	    bean.setFilter(new LoginCheckFilter());
+	    bean.addUrlPatterns("/*"); // 全部対象
+	    return bean;
 	}
 }
