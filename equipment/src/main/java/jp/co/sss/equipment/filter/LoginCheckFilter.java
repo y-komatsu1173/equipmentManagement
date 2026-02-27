dbf5bc78d1a5e8a3e581f6385151fa41af535e18
komatsu1173 <yu85wave@gmail.com>
2026-02-27 17:25:49 +0900

    fix:LoginCheckFilter.java RequestURI・ContextPathの追加

--- equipment/src/main/java/jp/co/sss/equipment/filter/LoginCheckFilter.java
+++ equipment/src/main/java/jp/co/sss/equipment/filter/LoginCheckFilter.java
@@ -18,28 +18,30 @@ public class LoginCheckFilter extends HttpFilter {
 
 	@Override
 	public void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
-	        throws IOException, ServletException {
+			throws IOException, ServletException {
 
-	    String path = request.getRequestURI();
+		String uri = request.getRequestURI();
+		String ctx = request.getContextPath();
+		String path = request.getRequestURI();
 
-	    //ログインページを外す
-	    if (path.equals("/") 
-	        || path.equals("/login") 
-	        || path.startsWith("/css/")
-	        || path.startsWith("/js/")
-	        || path.startsWith("/images/")) {
+		//ログインページを外す
+		if (path.equals("/")
+				|| path.equals("/login")
+				|| path.startsWith("/css/")
+				|| path.startsWith("/js/")
+				|| path.startsWith("/images/")) {
 
-	        chain.doFilter(request, response);
-	        return;
-	    }
+			chain.doFilter(request, response);
+			return;
+		}
 
-	    HttpSession session = request.getSession(false);
+		HttpSession session = request.getSession(false);
 
-	    if (session == null || session.getAttribute("user") == null) {
-	        response.sendRedirect("/");
-	        return;
-	    }
+		if (session == null || session.getAttribute("user") == null) {
+			response.sendRedirect("/");
+			return;
+		}
 
-	    chain.doFilter(request, response);
+		chain.doFilter(request, response);
 	}
 }
