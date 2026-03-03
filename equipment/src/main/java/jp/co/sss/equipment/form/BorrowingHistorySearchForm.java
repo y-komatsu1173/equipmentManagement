f218c2f4a27b9a1ea2c3efa3802194eb47c7fe7d
komatsu1173 <yu85wave@gmail.com>
2026-03-03 13:58:28 +0900

    add:BorrowingHistorySearchForm.java 履歴の絞り込みや条件検索用のフォームを追加

---
+++ equipment/src/main/java/jp/co/sss/equipment/form/BorrowingHistorySearchForm.java
@@ -0,0 +1,42 @@
+package jp.co.sss.equipment.form;
+
+import java.time.LocalDate;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+/**
+ * 貸出履歴検索フォーム
+ * 画面の絞り込み・並べ替え条件をまとめる
+ */
+public class BorrowingHistorySearchForm {
+
+	/*シリアルナンバー*/
+	private String stockCode;
+	
+	/*使用者(社員番号)*/
+	private Integer staffNo;
+	
+	/*貸出日*/
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	private LocalDate fromDate;
+	
+	/* 貸出日To*/
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	private LocalDate toDate;
+	
+	/*カテゴリ*/
+	private String categoryId;
+	
+	/*分類(会社所有 / リース)*/
+	private String ownershipType;
+	
+	/*削除済み　分別*/
+	private Boolean includeDeleted;
+	
+	/*並び替え*/
+	private String sortKey = "startDate";
+	
+	/*並び順*/
+	private String sortDir = "desc";
+	
+}
