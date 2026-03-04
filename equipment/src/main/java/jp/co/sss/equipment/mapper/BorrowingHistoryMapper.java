8164c20307c2d8fce6ee8a43c07d9f39b88def01
komatsu1173 <yu85wave@gmail.com>
2026-03-04 15:16:14 +0900

    fix:BorrowingHistoryMapper.java findBorrowingHistory引数の追加

--- equipment/src/main/java/jp/co/sss/equipment/mapper/BorrowingHistoryMapper.java
+++ equipment/src/main/java/jp/co/sss/equipment/mapper/BorrowingHistoryMapper.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 
 import jp.co.sss.equipment.dto.BorrowingHistoryDto;
+import jp.co.sss.equipment.form.BorrowingHistorySearchForm;
 
 /**
  * 貸出履歴を表示するマッパー
@@ -16,6 +17,6 @@ public interface BorrowingHistoryMapper {
 	 * 貸出履歴を呼び出す
 	 * @return
 	 */
-	List<BorrowingHistoryDto> findBorrowingHistory();
+	List<BorrowingHistoryDto> findBorrowingHistory(BorrowingHistorySearchForm form);
 
 }
