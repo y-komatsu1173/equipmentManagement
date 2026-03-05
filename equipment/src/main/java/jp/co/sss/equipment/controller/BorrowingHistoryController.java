87be02a000f2b1e966a717d5c4a84bd06b60dea3
komatsu1173 <yu85wave@gmail.com>
2026-03-05 13:05:43 +0900

    fix:BorrowingHistoryController.java カテゴリ取得とビュー表示の追加

--- equipment/src/main/java/jp/co/sss/equipment/controller/BorrowingHistoryController.java
+++ equipment/src/main/java/jp/co/sss/equipment/controller/BorrowingHistoryController.java
@@ -8,8 +8,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import jp.co.sss.equipment.dto.BorrowingHistoryDto;
+import jp.co.sss.equipment.entity.StockTypeMaster;
 import jp.co.sss.equipment.form.BorrowingHistorySearchForm;
 import jp.co.sss.equipment.service.BorrowingHistoryService;
+import jp.co.sss.equipment.service.EquipmentCommonService;
 
 /**
  * 備品管理貸出履歴
@@ -19,22 +21,30 @@ import jp.co.sss.equipment.service.BorrowingHistoryService;
 public class BorrowingHistoryController {
 	@Autowired
 	BorrowingHistoryService borrowingHistoryService;
+	
+	@Autowired
+	EquipmentCommonService equipmentCommonService;
 
 	/**
 	 * 履歴画面への遷移
 	 */
 	@GetMapping("/equipment/history")
 	public String equipmentHistoryView(BorrowingHistorySearchForm form, Model model) {
-		List<BorrowingHistoryDto> historyList =
-	            borrowingHistoryService.findBorrowingHistory(form);
+		List<BorrowingHistoryDto> historyList = borrowingHistoryService.findBorrowingHistory(form);
 
-	    //履歴確認
-	    for (BorrowingHistoryDto dto : historyList) {
-	        System.out.println(dto);
-	    }
+		//カテゴリの取得
+		List<StockTypeMaster> categoryList = equipmentCommonService.categoryFind();
 
-	    model.addAttribute("historyList", historyList);
-	    model.addAttribute("form",form);
+		//履歴確認
+		for (BorrowingHistoryDto dto : historyList) {
+			System.out.println(dto);
+		}
+
+		model.addAttribute("historyList", historyList);
+		model.addAttribute("categoryList", categoryList);
+		model.addAttribute("form", form);
+		
+		System.out.println("categoryList=" + categoryList);
 
 		return "history/borrowingHistory";
 	}
