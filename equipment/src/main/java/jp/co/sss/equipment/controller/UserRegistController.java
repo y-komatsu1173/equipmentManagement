c789400e0aafaa8aa18b61a93f147f10dd7afb2e
komatsu1173 <yu85wave@gmail.com>
2026-03-17 10:10:40 +0900

    add:UserRegistController.java　ユーザー登録コントローラの追加

---
+++ equipment/src/main/java/jp/co/sss/equipment/controller/UserRegistController.java
@@ -0,0 +1,83 @@
+package jp.co.sss.equipment.controller;
+
+import java.util.List;
+
+import jakarta.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import jp.co.sss.equipment.entity.AuthMaster;
+import jp.co.sss.equipment.form.UserRegistForm;
+import jp.co.sss.equipment.service.StaffCommonService;
+
+/**
+ * ユーザー登録
+ */
+@Controller
+public class UserRegistController {
+	@Autowired
+	StaffCommonService staffCommonService;
+
+	/**
+	 * ユーザー登録画面
+	 */
+	@GetMapping("user/regist/input")
+	public String registInput(Model model) {
+		//権限情報の取得
+		List<AuthMaster> authList = staffCommonService.authFind();
+		model.addAttribute("authList", authList);
+		model.addAttribute("userRegistForm", new UserRegistForm());
+
+		System.out.println(authList);
+		return "userRegist/userInput";
+	}
+	
+	/**
+	 * 入力確認画面
+	 */
+	@PostMapping("user/regist/check")
+	public String registCheck(
+			@Valid @ModelAttribute UserRegistForm registform,
+			BindingResult result,
+			RedirectAttributes redirectAttributes,
+			Model model) {
+		//権限情報の取得
+		List<AuthMaster> authList = staffCommonService.authFind();
+		model.addAttribute("authList", authList);
+
+		// 入力チェック
+		if (result.hasErrors()) {
+			return "userRegist/input";
+		}
+		
+		//権限IDから権限情報を取得
+		AuthMaster authMaster = null;
+
+		//権限IDがnullでない場合、権限情報を取得
+		if (registform.getAuth() != null) {
+			authMaster = staffCommonService.authFindById(registform.getAuth());
+			model.addAttribute("authMaster", authMaster);
+		}
+		//ユーザー登録情報をセッションに保存
+		model.addAttribute("userRegistForm", registform);
+
+		return "userRegist/check";
+	}
+	
+	/*
+	 * 登録処理(完了画面)
+	 */
+	@PostMapping("user/regist/complete")
+	public String registComplete() {
+		
+		return "userRegist/complete";
+	}
+	
+}
