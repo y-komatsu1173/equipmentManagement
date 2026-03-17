7aa192a1bdd7132877ec4ebdd14536fd1eeaf704
komatsu1173 <yu85wave@gmail.com>
2026-03-17 10:16:52 +0900

    fix:StaffCommonService.java 権限検索のメソッドを追加

--- equipment/src/main/java/jp/co/sss/equipment/service/StaffCommonService.java
+++ equipment/src/main/java/jp/co/sss/equipment/service/StaffCommonService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import jp.co.sss.equipment.entity.AuthMaster;
 import jp.co.sss.equipment.entity.StaffData;
 import jp.co.sss.equipment.mapper.StaffCommonMapper;
 
@@ -29,6 +30,23 @@ public class StaffCommonService {
 	 */
 	public StaffData staffFindIndividual(Integer staffNo) {
 		return staffCommonMapper.staffFindIndividual(staffNo);
+	}
+
+	/**
+	 * 権限の取得
+	 * @return
+	 */
+	public List<AuthMaster> authFind() {
+		return staffCommonMapper.authFind();
+	}
+
+	/**
+	 * 権限のカテゴリ一個を取得
+	 * @param auth
+	 * @return
+	 */
+	public AuthMaster authFindById(Integer authNo) {
+		return staffCommonMapper.authFindById(authNo);
 	}
 	
 }
