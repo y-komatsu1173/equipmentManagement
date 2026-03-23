b9448590f8d4f2577c237efed4795d8110263b59
komatsu1173 <yu85wave@gmail.com>
2026-03-23 13:25:29 +0900

    fix:UserUpdateService.java UserForm.javaのクラス名変更による引数の変更

--- equipment/src/main/java/jp/co/sss/equipment/service/UserUpdateService.java
+++ equipment/src/main/java/jp/co/sss/equipment/service/UserUpdateService.java
@@ -2,10 +2,16 @@ package jp.co.sss.equipment.service;
 
 import org.springframework.stereotype.Service;
 
+import jp.co.sss.equipment.form.UserForm;
+
 /**
  * ユーザー情報編集サービス
  */
 @Service
 public class UserUpdateService {
+
+	public void userUpdate(UserForm updateForm) {
+		
+	}
 
 }
