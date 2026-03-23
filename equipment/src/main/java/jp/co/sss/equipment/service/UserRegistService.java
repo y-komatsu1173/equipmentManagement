6c814692223ec55a4626cc14ef4127ea146e8205
komatsu1173 <yu85wave@gmail.com>
2026-03-23 13:24:39 +0900

    fix:UserRegistService.java UserForm.javaのクラス名変更による引数の変更

--- equipment/src/main/java/jp/co/sss/equipment/service/UserRegistService.java
+++ equipment/src/main/java/jp/co/sss/equipment/service/UserRegistService.java
@@ -1,13 +1,11 @@
 package jp.co.sss.equipment.service;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import jp.co.sss.equipment.entity.StaffData;
-import jp.co.sss.equipment.form.UserRegistForm;
+import jp.co.sss.equipment.form.UserForm;
 import jp.co.sss.equipment.mapper.StaffCommonMapper;
 import jp.co.sss.equipment.mapper.UserRegistMapper;
 import jp.co.sss.equipment.util.BeanCopy;
@@ -27,7 +25,7 @@ public class UserRegistService {
 	 *ユーザー登録処理
 	 */
 	@Transactional
-	public void userRegistInsert(UserRegistForm registform) {
+	public void userRegistInsert(UserForm registform) {
 		//formからentityへコピー
 		StaffData staffData = BeanCopy.userCopyEntity(registform);
 		//論理削除フラグを0に
@@ -36,20 +34,4 @@ public class UserRegistService {
 		userRegistMapper.userRegistInsert(staffData);
 	}
 
-	/**
-	 * IDの重複チェック
-	 * @param staffNo
-	 * @return
-	 */
-	public boolean idCheck(Integer staffNo) {
-		//ユーザーの取得
-		List<StaffData> staffData = staffCommonMapper.staffFind();
-		//idの一致検索
-		for (StaffData staff : staffData) {
-			if (staff.getStaffNo().equals(staffNo)) {
-				return true; // 重複あり
-			}
-		}
-		return false; // 重複なし
-	}
 }
