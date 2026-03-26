2fdd63267cef15fb6f130b41956a1a6103a9b482
komatsu1173 <yu85wave@gmail.com>
2026-03-26 10:05:58 +0900

    fix:UserUpdateService.java パスワードチェックメソッドを追加

--- equipment/src/main/java/jp/co/sss/equipment/service/UserUpdateService.java
+++ equipment/src/main/java/jp/co/sss/equipment/service/UserUpdateService.java
@@ -1,8 +1,13 @@
 package jp.co.sss.equipment.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import jp.co.sss.equipment.dto.PasswordCheckDto;
+import jp.co.sss.equipment.entity.StaffData;
 import jp.co.sss.equipment.form.UserForm;
+import jp.co.sss.equipment.mapper.StaffCommonMapper;
+import jp.co.sss.equipment.mapper.UserUpdateMapper;
 
 /**
  * ユーザー情報編集サービス
@@ -10,8 +15,55 @@ import jp.co.sss.equipment.form.UserForm;
 @Service
 public class UserUpdateService {
 
-	public void userUpdate(UserForm updateForm) {
-		
+	@Autowired
+	UserUpdateMapper userUpdateMapper;
+	
+	@Autowired
+	StaffCommonMapper staffCommonMapper;
+	/**
+	 * 更新処理
+	 * @param updateForm
+	 */
+	public int userUpdate(UserForm updateForm) {
+		return userUpdateMapper.userUpdate(updateForm);
+	}
+	
+	/**
+	 * パスワードチェック
+	 * @param form
+	 * @param loginUser
+	 * @return
+	 */
+	public PasswordCheckDto checkPasswordUpdate(UserForm form, StaffData loginUser) {
+
+		PasswordCheckDto dto = new PasswordCheckDto();
+
+		// 新しいパスワード未入力ならチェック不要
+		if (form.getPassword() == null || form.getPassword().isEmpty()) {
+			return dto;
+		}
+
+		// 自分以外のパスワード変更は禁止
+		if (!loginUser.getStaffNo().equals(form.getOldStaffNo())) {
+			dto.setPasswordChangeNotAllowed(true);
+			return dto;
+		}
+
+		// 現在のパスワード未入力
+		if (form.getCurrentPassword() == null || form.getCurrentPassword().isEmpty()) {
+			dto.setCurrentPasswordRequired(true);
+			return dto;
+		}
+
+		// DBのパスワード取得
+		StaffData dbUser = staffCommonMapper.staffFindIndividual(form.getOldStaffNo());
+
+		// 現在のパスワード不一致
+		if (!dbUser.getPassword().equals(form.getCurrentPassword())) {
+			dto.setCurrentPasswordInvalid(true);
+		}
+
+		return dto;
 	}
 
 }
