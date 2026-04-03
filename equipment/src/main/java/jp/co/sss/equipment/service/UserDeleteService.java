35c2e23e50ab7e1000c66508e81a255c4086f10d
komatsu1173 <yu85wave@gmail.com>
2026-04-03 14:58:10 +0900

    fix:UserDeleteService.java ユーザーの論理削除メソッドを追加

--- equipment/src/main/java/jp/co/sss/equipment/service/UserDeleteService.java
+++ equipment/src/main/java/jp/co/sss/equipment/service/UserDeleteService.java
@@ -2,6 +2,7 @@ package jp.co.sss.equipment.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import jp.co.sss.equipment.mapper.UserDeleteMapper;
 
@@ -11,8 +12,15 @@ import jp.co.sss.equipment.mapper.UserDeleteMapper;
 
 @Service
 public class UserDeleteService {
-	
-@Autowired
-UserDeleteMapper userDeleteMapper;
-	
+
+	@Autowired
+	UserDeleteMapper userDeleteMapper;
+
+	//ユーザー削除（論理削除）
+	@Transactional
+	public void userDelete(Integer staffNo) {
+		userDeleteMapper.userDelete(staffNo);
+
+	}
+
 }
