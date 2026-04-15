b3d264c08ffcbf4afc8e540aaabe1581a2f57c62
komatsu1173 <yu85wave@gmail.com>
2026-04-15 17:50:58 +0900

    add:BorrowingHistoryController.java ユーザー認証を追加し、一般ユーザーの社員番号を設定

--- equipment/src/main/java/jp/co/sss/equipment/controller/BorrowingHistoryController.java
+++ equipment/src/main/java/jp/co/sss/equipment/controller/BorrowingHistoryController.java
@@ -2,12 +2,15 @@ package jp.co.sss.equipment.controller;
 
 import java.util.List;
 
+import jakarta.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import jp.co.sss.equipment.dto.BorrowingHistoryDto;
+import jp.co.sss.equipment.entity.StaffData;
 import jp.co.sss.equipment.entity.StockTypeMaster;
 import jp.co.sss.equipment.form.BorrowingHistorySearchForm;
 import jp.co.sss.equipment.service.BorrowingHistoryService;
@@ -29,23 +32,32 @@ public class BorrowingHistoryController {
 	 * 履歴画面への遷移
 	 */
 	@GetMapping("/equipment/history")
-	public String equipmentHistoryView(BorrowingHistorySearchForm form, Model model) {
+	public String equipmentHistoryView(BorrowingHistorySearchForm form, Model model, HttpSession session) {
+		StaffData loginUser = (StaffData) session.getAttribute("user");
+
+	    // 未ログイン対策
+	    if (loginUser == null) {
+	        return "redirect:/";
+	    }
+
+	    // 一般ユーザーは自分の社員番号で固定
+	    if (loginUser.getAuthNo() == 99) {
+	        form.setStaffNo(loginUser.getStaffNo());
+	    }
+		
 		List<BorrowingHistoryDto> historyList = borrowingHistoryService.findBorrowingHistory(form);
 
 		//カテゴリの取得
 		List<StockTypeMaster> categoryList = equipmentCommonService.categoryFind();
 
 		//履歴確認
-		for (BorrowingHistoryDto dto : historyList) {
-			System.out.println(dto);
-		}
+		
 
 		model.addAttribute("historyList", historyList);
 		model.addAttribute("categoryList", categoryList);
 		model.addAttribute("form", form);
 		
-		System.out.println("categoryList=" + categoryList);
-
+		
 		return "history/borrowingHistory";
 	}
 }
