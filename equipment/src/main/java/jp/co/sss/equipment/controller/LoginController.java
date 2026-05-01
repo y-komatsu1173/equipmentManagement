f6ad98d13d6ac1289489bbabb27bc60f2914fe08
komatsu1173 <yu85wave@gmail.com>
2026-05-01 17:44:07 +0900

    add: LoginController.java OTP認証機能を追加し、ログイン処理を修正

--- equipment/src/main/java/jp/co/sss/equipment/controller/LoginController.java
+++ equipment/src/main/java/jp/co/sss/equipment/controller/LoginController.java
@@ -10,9 +10,8 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
+import jp.co.sss.equipment.entity.StaffData;
 import jp.co.sss.equipment.form.LoginForm;
 import jp.co.sss.equipment.service.IndexService;
 import jp.co.sss.equipment.service.LoginResult;
@@ -41,9 +40,16 @@ public class LoginController {
 	 * @author 小松原
 	 * @return　templates/index/index ログイン画面
 	 */
+//	@GetMapping("/")
+//	public String loginForm(LoginForm loginForm) {
+//	    session.invalidate();
+//	    return "login";
+//	}
+	
 	@GetMapping("/")
-	public String loginForm(LoginForm loginForm) {
+	public String loginForm(Model model) {
 	    session.invalidate();
+	    model.addAttribute("loginForm", new LoginForm());
 	    return "login";
 	}
 
@@ -53,30 +59,65 @@ public class LoginController {
 	@PostMapping("/login")
 	public String login(@Valid @ModelAttribute LoginForm loginForm, BindingResult result, HttpSession session, Model model) {
 	    
+//		if (result.hasErrors()) {
+//			return "login";
+//		}
+//
+//		LoginResult loginResult = loginService.excute(loginForm);
+//		//TODO loginResult.isLoginの結果がtrueの場合、ログイン成功でセッションに"user"という名前でセッションにユーザーの情報を登録する
+//		if (loginResult.isLogin()) {
+//			//セッションにuser登録
+//			session.setAttribute("user", loginResult.getLoginUser());
+//			
+//			 int authNo = loginResult.getLoginUser().getAuthNo();
+//			//権限者の場合
+//			  if (authNo != 99) {
+//		            return "topMenu";
+//		        }
+//
+//			// 一覧へリダイレクト
+//			return "redirect:/index";
+		//loginResult.isLoginの結果がfalseの場合、loginResult.getErrorMsgメソッドを呼びだし、modelスコープに登録する
+//		} else {//ログイン失敗時
+//			//loginResult.getErrorMsgを呼び出し、メッセージをmodelスコープに登録
+//			model.addAttribute("errMessage", loginResult.getErrorMsg());
+//			return "login";
+//		}
+		
 		if (result.hasErrors()) {
-			return "login";
-		}
 
-		LoginResult loginResult = loginService.excute(loginForm);
-		//TODO loginResult.isLoginの結果がtrueの場合、ログイン成功でセッションに"user"という名前でセッションにユーザーの情報を登録する
-		if (loginResult.isLogin()) {
-			//セッションにuser登録
-			session.setAttribute("user", loginResult.getLoginUser());
-			
-			 int authNo = loginResult.getLoginUser().getAuthNo();
-			//権限者の場合
-			  if (authNo != 99) {
-		            return "topMenu";
-		        }
+	        return "login";
 
-			// 一覧へリダイレクト
-			return "redirect:/index";
-			//loginResult.isLoginの結果がfalseの場合、loginResult.getErrorMsgメソッドを呼びだし、modelスコープに登録する
-		} else {//ログイン失敗時
-			//loginResult.getErrorMsgを呼び出し、メッセージをmodelスコープに登録
-			model.addAttribute("errMessage", loginResult.getErrorMsg());
-			return "login";
-		}
+	    }
+
+	    LoginResult loginResult = loginService.excute(loginForm);
+
+	    if (loginResult.isLogin()) {
+
+	        // OTP発行
+
+	        otpAuthService.issueOtp(
+
+	            loginResult.getLoginUser().getStaffNo(),
+
+	            loginResult.getLoginUser().getMail()
+
+	        );
+
+	        // 一時保持
+
+	        session.setAttribute("otpUser", loginResult.getLoginUser());
+
+	        return "redirect:/oneTime";
+
+	    }
+
+	    // これが必要ログイン失敗時
+
+	    model.addAttribute("errMessage", loginResult.getErrorMsg());
+
+	    return "login";
+
 	}
 	
 	/**
@@ -92,21 +133,48 @@ public class LoginController {
 	 * ログアウト処理
 	 * @return
 	 */
-	@RequestMapping(path = "/logout", method = RequestMethod.GET)
-	public String logout() {
-		//セッションの破棄
-		session.invalidate();
+	@GetMapping("/logout")
+	public String logout(HttpSession session) {
 
-		//index.htmlへ遷移
-		return "redirect:/";
+	    if (session != null) {
+	        session.invalidate();
+	    }
+
+	    return "redirect:/";
 	}
 	
 	/**
 	 * ワンタイムパスコード入力画面
 	 */
-	@PostMapping("oneTime")
+	@GetMapping("/oneTime")
 	public String onetime() {
-		return "onetime";
+	    return "oneTime";
+	}
+	
+	@PostMapping("/otpCheck")
+	public String otpCheck(String otp, HttpSession session, Model model) {
+
+	    var user = (StaffData) session.getAttribute("otpUser");
+
+	    if (user == null) {
+	        return "redirect:/";
+	    }
+
+	    boolean result = otpAuthService.verifyOtp(
+	        user.getStaffNo(),
+	        otp
+	    );
+
+	    if (result) {
+	        // ログイン確定
+	        session.setAttribute("user", user);
+	        session.removeAttribute("otpUser");
+
+	        return "redirect:/index";
+	    } else {
+	        model.addAttribute("error", "コードが違います");
+	        return "oneTime";
+	    }
 	}
 	
 }
