6be9529292a64d506e2212a2ecdac51bf3ae96ac
komatsu1173 <yu85wave@gmail.com>
2026-05-12 15:06:47 +0900

    feat: EquipmentSearchService.java 検索条件が入力されているか判定するhasSearchConditionメソッドを追加

--- equipment/src/main/java/jp/co/sss/equipment/service/EquipmentSearchService.java
+++ equipment/src/main/java/jp/co/sss/equipment/service/EquipmentSearchService.java
@@ -18,9 +18,16 @@ public class EquipmentSearchService {
 
 	@Autowired
 	EquipmentSearchMapper equipmentSearchMapper;
-	
+
 	public List<EquipmentSearchDto> search(EquipmentSearchForm form) {
 		return equipmentSearchMapper.search(form);
 	}
 
+	// 検索条件が1つでも入力されているか判定
+	public boolean hasSearchCondition(EquipmentSearchForm form) {
+		return (form.getName() != null && !form.getName().isBlank()) ||
+				form.getStockType() != null ||
+				(form.getStatus() != null && !form.getStatus().isBlank()) ||
+				form.getOwnershipType() != null;
+	}
 }
