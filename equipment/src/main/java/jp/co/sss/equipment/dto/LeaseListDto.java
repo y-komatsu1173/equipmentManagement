9c0e46524e5e1e76a69c10b917f9a44ec7e23725
komatsu1173 <yu85wave@gmail.com>
2026-05-19 15:06:16 +0900

    add:LeaseListDto 新しいプロパティを追加し、リース情報の詳細を強化

--- equipment/src/main/java/jp/co/sss/equipment/dto/LeaseListDto.java
+++ equipment/src/main/java/jp/co/sss/equipment/dto/LeaseListDto.java
@@ -8,25 +8,50 @@ import lombok.Data;
 
 @Data
 public class LeaseListDto {
-	
+
 	/**シリアル*/
 	private String stockCode;
-	
+
 	/**リース返却日*/
 	private String leaseReturnDate;
-	
+
 	/**製品名*/
 	private String productName;
-	
+
 	/**カテゴリ*/
 	private String categoryName;
-	
+
 	/**貸出状態*/
-    private String statusName;
-	
+	private String statusName;
+
 	/**返却期限*/
 	private String returnDate;
-	
+
 	/**使用者*/
 	private String staffName;
+
+	/** 行色用CSSクラス */
+	private String rowClass;
+
+	/** 1週間以内フラグ */
+	private boolean nearLimit;
+
+	/** 期限切れフラグ */
+	private boolean overdue;
+
+	/** メール */
+	private String mail;
+
+	/**通知種別*/
+	private String alertType;
+
+	/**通知メッセージ*/
+	private String alertMessage;
+
+	/** 返却期限残日数 */
+	private Integer remainingDays;
+
+	/** リース返却残日数 */
+	private Integer remainingLeaseDays;
+
 }
