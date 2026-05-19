aabd251b1e33f93c507e2c3707dc9e1f81123e53
komatsu1173 <yu85wave@gmail.com>
2026-05-19 15:06:32 +0900

    add:LeaseMapper リース返却通知機能を追加し、1ヶ月前、1週間前、1日前の通知を取得するメソッドを実装

--- equipment/src/main/java/jp/co/sss/equipment/mapper/LeaseMapper.java
+++ equipment/src/main/java/jp/co/sss/equipment/mapper/LeaseMapper.java
@@ -18,4 +18,34 @@ public interface LeaseMapper {
 	 * リース製品一覧取得
 	 */
 	public List<LeaseListDto> leaseListSertch(LeaseSearchForm form);
+
+	/**
+	 * リース1ヶ月前
+	 */
+	List<LeaseListDto> findLeaseReturnAfterOneMonth();
+
+	/**
+	 * リース返却1週間前
+	 */
+	List<LeaseListDto> findLeaseReturnAfterOneWeek();
+
+	/**
+	 * リース返却1週間前
+	 */
+	List<LeaseListDto> findLeaseReturnAfterOneDay();
+
+	/**
+	 * リース通知の取得
+	 */
+	List<LeaseListDto> findLeaseAlerts();
+	
+	/**
+	 * リース通知の取得
+	 */
+	List<LeaseListDto> findLeaseAlerts(Integer staffNo);
+	
+	/**
+	 * 一般ユーザー通知
+	 */
+	List<LeaseListDto> findUserLeaseAlerts(Integer staffNo);
 }
